9d35c1a655aaaaaf2ef0eea92053d844a081df58
Ryan Ureta <rcureta95@gmail.com>
2018-07-20 09:42:07 -0700

    finished delete functionality

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const STORE = [
 	{name:'milk',checked:true},
 	{name:'bread',checked:true}
 ];
-
+//--------------------------------------------------------
+//start Render
+//--------------------------------------------------------
 
 function generateItemElement(item, itemIndex, template) {
   return `
@@ -37,27 +39,71 @@ function renderShoppingList() {
   const shoppingListItemsString = generateListHtml(STORE);
   $('.js-shopping-list').html(shoppingListItemsString);
 }
+//-----------------------------------------------------------------------------------
+//end render, start submit
+//-----------------------------------------------------------------------------------
 
+function addItemToShoppingList(itemName) {
+  console.log(`Adding "${itemName}" to shopping list`);
+  STORE.push({name: itemName, checked: false});
+}
 
-function newItemSubmit(){
-	console.log('Submit');
+function handleNewItemSubmit() {
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    console.log('`handleNewItemSubmit` ran');
+    const newItemName = $('.js-shopping-list-entry').val();
+    $('.js-shopping-list-entry').val('');
+    addItemToShoppingList(newItemName);
+    renderShoppingList();
+  });
+}
+//---------------------------------------------------
+//end submit, start check
+//---------------------------------------------------
+
+function toggleCheckedForListItem(itemIndex){STORE[itemIndex].checked = !STORE[itemIndex].checked;}
+
+function getItemIndexFromElement(item){
+	const itemIndexString=$(item)
+			       .closest('.js-item-index-element')
+			       .attr('data-item-index');
+	return parseInt(itemIndexString,10);
 }
 
 function itemCheckClicked(){
-
 	console.log('checking');
+	$('.js-shopping-list').on('click','.js-item-toggle', event => {
+		const itemIndex=getItemIndexFromElement(event.currentTarget);
+		toggleCheckedForListItem(itemIndex);
+		renderShoppingList();
+	});
+}
+//-------------------------
+//end check, start delete
+//-------------------------
+
+function removeFromList(x){
+	STORE.splice(x,1);
 }
 
 function itemDeleteClicked(){
-
 	console.log('delete');
+	$('.js-shopping-list').on('click','.js-item-delete', event => {
+		const x = getItemIndexFromElement(event.currentTarget);
+		removeFromList(x);
+		renderShoppingList();
+	});
 }
 
 function handleShoppingList(){
 	renderShoppingList();
-	newItemSubmit();
+	handleNewItemSubmit();
 	itemCheckClicked();
 	itemDeleteClicked();
 }
 
+/*---------------------------------
+end
+---------------------------------*/
 $(handleShoppingList);
