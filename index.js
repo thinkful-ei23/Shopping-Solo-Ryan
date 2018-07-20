6e16ab916ec35fe309eebccacb92f25d057a7139
Ryan Ureta <rcureta95@gmail.com>
2018-07-20 08:51:29 -0700

    render

--- index.js
+++ index.js
@@ -7,14 +7,39 @@ const STORE = [
 	{name:'bread',checked:true}
 ];
 
-function renderShoppingList(){
-//DOM render
-	console.log('render');
 
+function generateItemElement(item, itemIndex, template) {
+  return `
+    <li class="js-item-index-element" data-item-index="${itemIndex}">
+      <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle js-item-toggle">
+            <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete js-item-delete">
+            <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>
+    `;
 }
 
-function newItemSubmit(){
 
+function generateListHtml(shoppingList) {
+  console.log("Generating shopping list element");
+  const items = shoppingList.map((item, index) => generateItemElement(item, index));
+  return items.join("");
+}
+
+
+function renderShoppingList() {
+  console.log('`renderShoppingList` ran');
+  const shoppingListItemsString = generateListHtml(STORE);
+  $('.js-shopping-list').html(shoppingListItemsString);
+}
+
+
+function newItemSubmit(){
 	console.log('Submit');
 }
 
